d79bc323a6a342472b6a084c43932852062c70a7
i340771 <naresh.ganta@sap.com>
2026-03-11 20:12:36 +0530

    updating code

--- infra-helm-charts/provider.tf
+++ infra-helm-charts/provider.tf
@@ -1,9 +1,21 @@
+provider "helm" {
+  kubernetes = {
+    config_path = "~/.kube/config"
+  }
+}
+
 provider "azurerm" {
   features {}
   subscription_id = "ddffee8a-e239-4aa1-b7e0-b88ff5a2f9aa"
 }
 terraform {
   backend "azurerm" {}
+  required_providers {
+    vault = {
+      source  = "hashicorp/vault"
+      version = "4.8.0"
+    }
+  }
 }
 
 provider "vault" {
