90a5bbeb81b75d79a020fde834f56e5868764455
i340771 <naresh.ganta@sap.com>
2026-04-05 13:46:51 +0530

    updating code

--- infra-helm-charts/modules/helm/main.tf
+++ infra-helm-charts/modules/helm/main.tf
@@ -236,7 +236,7 @@ resource "helm_release" "istiod" {
   name             = "istiod"
   repository       = "https://istio-release.storage.googleapis.com/charts"
   chart            = "istiod"
-  namespace        = "istio-system"
+  namespace        = "istio-system  "
   create_namespace = true
   version          = "1.27.0"
 }
